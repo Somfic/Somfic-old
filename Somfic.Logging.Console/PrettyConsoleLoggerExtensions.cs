41b7d463fdc2822a620f68fe9b8cfc92517b9e59
Somfic <somfic@gmail.com>
2020-08-13 15:23:53 +0200

    Added AddPrettyConsole() extension method

---
+++ Somfic.Logging.Console/PrettyConsoleLoggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using Somfic.Logging.Console.Themes;
+
+namespace Somfic.Logging.Console
+{
+    public static class PrettyConsoleLoggerExtensions
+    {
+        public static ILoggingBuilder AddPrettyConsole(this ILoggingBuilder builder, IConsoleTheme theme = null)
+        {
+            if(theme == null) { theme = ConsoleThemes.Code; }
+
+            builder.AddProvider(new PrettyConsoleLoggerProvider(theme));
+
+            return builder;
+        }
+    }
+}
