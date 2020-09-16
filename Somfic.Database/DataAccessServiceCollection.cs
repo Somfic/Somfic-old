e824514f5ae834e3fc5e89371651ce1d32d2b4cd
Somfic <somfic@gmail.com>
2020-09-16 11:24:32 +0200

    Add AddDataAccess()

---
+++ Somfic.Database/DataAccessServiceCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Somfic.Database
+{
+    public static class DataAccessServiceCollection
+    {
+        /// <summary>
+        /// Adds an <seealso cref="IDataAccess"/> class to the <seealso cref="IServiceCollection"/>
+        /// </summary>
+        /// <typeparam name="T">The <see cref="IDataAccess"/> implementation</typeparam>
+        /// <returns></returns>
+        public static IServiceCollection AddDataAccess<T>(this IServiceCollection services) where T : class, IDataAccess
+        {
+            services.AddSingleton<IDataAccess, T>();
+
+            return services;
+        }
+    }
+}
