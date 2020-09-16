41b5ab37e0a4b48f81c2ae44824f57478e0af6d9
Somfic <somfic@gmail.com>
2020-09-16 10:53:53 +0200

    Added Somfic.Database.Sql

---
+++ Somfic.Database.SQL/SqlDataAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Somfic.Database.Sql
+{
+    public class SqlDataAccess : DataAccess
+    {
+        public SqlDataAccess(ILogger log, IConfiguration config) : base(log, config)
+        {
+        }
+
+        protected override IDbConnection MakeDbConnection(string connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
